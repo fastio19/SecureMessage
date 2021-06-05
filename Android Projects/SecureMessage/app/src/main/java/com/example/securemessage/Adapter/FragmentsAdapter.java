102af0d0d584bdbee835b5053829b179fe11077c
Bhavik <bhavik.diwani2000@gmail.com>
2021-06-05 23:15:21 +0530

    first commit

--- Android Projects/SecureMessage/app/src/main/java/com/example/securemessage/Adapter/FragmentsAdapter.java
+++ Android Projects/SecureMessage/app/src/main/java/com/example/securemessage/Adapter/FragmentsAdapter.java
@@ -1,2 +1,59 @@
-package com.example.securemessage.Adapter;public class FragmentsAdapter {
+package com.example.securemessage.Adapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.securemessage.Fragments.CallsFragment;
+import com.example.securemessage.Fragments.ChatsFragment;
+import com.example.securemessage.Fragments.StatusFragment;
+
+import org.jetbrains.annotations.NotNull;
+
+public class FragmentsAdapter extends FragmentPagerAdapter {
+    public FragmentsAdapter(@NonNull @NotNull FragmentManager fm) {
+        super(fm);
+    }
+
+    public FragmentsAdapter(@NonNull @NotNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:return new ChatsFragment();
+            case 1:return new StatusFragment();
+            case 2:return new CallsFragment();
+            default:return new ChatsFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Nullable
+    @org.jetbrains.annotations.Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title=null;
+        if(position==0){
+            title="CHATS";
+        }
+
+        if(position==1){
+            title="STATUS";
+        }
+
+        if(position==2){
+            title="CALLS";
+        }
+        return title;
+    }
 }
--- Android Projects/SecureMessage/app/src/main/java/com/example/securemessage/SignUpActivity.java
+++ Android Projects/SecureMessage/app/src/main/java/com/example/securemessage/SignUpActivity.java
@@ -1,14 +1,79 @@
 package com.example.securemessage;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.securemessage.Models.Users;
+import com.example.securemessage.databinding.ActivitySignUpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.jetbrains.annotations.NotNull;
 
 public class SignUpActivity extends AppCompatActivity {
-
+    ActivitySignUpBinding binding;
+    private FirebaseAuth auth;
+    FirebaseDatabase database;
+    ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding=ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getSupportActionBar().hide();
+
+        auth=FirebaseAuth.getInstance();
+        database=FirebaseDatabase.getInstance();
+        progressDialog=new ProgressDialog(SignUpActivity.this);
+        progressDialog.setTitle("Creating Account");
+        progressDialog.setMessage("We're creating your account");
+        binding.btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressDialog.show();
+                auth.createUserWithEmailAndPassword
+                        (binding.etEmail.getText().toString(),
+                                binding.etPassword.getText().toString()).
+                        addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+                        if(task.isSuccessful()){
+                            Users user=new Users(binding.etUserName.getText().toString(),
+                                    binding.etEmail.getText().toString(),
+                                    binding.etPassword.getText().toString());
+                            String id=task.getResult().getUser().getUid();
+                            user.setUserId(id);
+                            database.getReference().child("Users").child(id).setValue(user);
+                            Toast.makeText(SignUpActivity.this,"User created Successfully",
+                                    Toast.LENGTH_SHORT).show();
+                            Intent intent=new Intent(SignUpActivity.this,SignInActivity.class);
+                            startActivity(intent);
+                        }
+                        else{
+                            Toast.makeText(SignUpActivity.this,task.getException().getMessage(),
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+        binding.alreadyHaveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(SignUpActivity.this,SignInActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
