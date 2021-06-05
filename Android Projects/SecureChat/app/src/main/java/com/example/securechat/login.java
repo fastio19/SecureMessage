b67ca41dfb05f436b9456bb34877e20f3638fdcc
Bhavik <bhavik.diwani2000@gmail.com>
2021-06-05 23:12:03 +0530

    Initial commit

---
+++ Android Projects/SecureChat/app/src/main/java/com/example/securechat/login.java
@@ -0,0 +1,13 @@
+package com.example.securechat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ Android Projects/SecureMessage/app/src/main/java/com/example/securemessage/Adapter/FragmentsAdapter.java
@@ -0,0 +1,2 @@
+package com.example.securemessage.Adapter;public class FragmentsAdapter {
+}
